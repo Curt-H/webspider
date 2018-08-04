f3efe4a25cc9b64471bd23a6981dadfccc948b0b
Curt_H <Curt_H@126.com>
2018-08-04 12:51:46 +0800

    增加: 缓存功能

--- douban_spider.py
+++ douban_spider.py
@@ -16,5 +16,37 @@ class Movie(object):
         self.cover_url = ""
 
 
+def cached_url(url, filename):
+    """
+
+    :param url:
+    :param filename:
+    :return:
+    """
+    u = url
+    fname = filename
+
+    folder = 'cache'
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    path = os.path.join(folder, filename)
+    if os.path.exists(path):
+        with open(path, 'rb') as f:
+            s = f.read()
+            return s
+    else:
+        r = requests.get(url)
+        with open(path, 'wb') as f:
+            f.write(r.content)
+            return r.content
+
+
+def save_pic(movies):
+    for path in movies:
+        filename = f'{path.name}.png'
+        cached_url(path.cover_url, filename)
+
+
 if __name__ == '__main__':
     pass
