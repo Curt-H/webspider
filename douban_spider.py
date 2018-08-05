f3ac0790e283343532aa1598a455e7907709976a
Curt_H <Curt_H@126.com>
2018-08-05 14:25:38 +0800

    新增: url生成和基本pq功能

--- douban_spider.py
+++ douban_spider.py
@@ -7,6 +7,8 @@ import os
 import requests
 from pyquery import PyQuery as pq
 
+from utils import log
+
 
 class Movie(object):
     def __init__(self):
@@ -14,6 +16,12 @@ class Movie(object):
         self.score = ""
         self.quote = ""
         self.cover_url = ""
+
+
+def collect_data(content):
+    e = pq(content)
+    items = e(".item")
+    return items
 
 
 def cached_url(url, filename):
@@ -48,5 +56,22 @@ def save_pic(movies):
         cached_url(path.cover_url, filename)
 
 
+def get_all_page_url():
+    urls = list()
+    for i in range(0, 250, 25):
+        url = f"https://movie.douban.com/top250?start={i}"
+        urls.append(url)
+    return urls
+
+
+def __main():
+    url = get_all_page_url()
+    for i, u in enumerate(url):
+        log(f'URL is {u}')
+
+        content = cached_url(u, f'page{i}')
+        print(collect_data(content))
+
+
 if __name__ == '__main__':
-    pass
+    __main()
