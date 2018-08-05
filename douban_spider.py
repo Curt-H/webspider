eb0ada225531fdff778c97525f55d7f965791879
Curt_H <Curt_H@126.com>
2018-08-05 15:30:54 +0800

    新增: 豆瓣250爬虫数据分析, 记录, 导出md文件的功能

--- douban_spider.py
+++ douban_spider.py
@@ -15,13 +15,46 @@ class Movie(object):
         self.name = ""
         self.score = ""
         self.quote = ""
+        self.hot = ""
         self.cover_url = ""
+
+    def __repr__(self):
+        attr_list = [f"{k}: {v}" for k, v in self.__dict__.items()]
+        result = '\n'.join(attr_list)
+        return result
+
+    def save(self):
+        fname = self.name.split('/')[0].replace(' ', '')
+
+        with open(f'cache\\{fname}.md', 'w', encoding='utf-8') as f:
+            f.write(self.__repr__())
+            f.write(f'\r\n![{fname}]({fname}.jpg)')
 
 
 def collect_data(content):
     e = pq(content)
     items = e(".item")
-    return items
+
+    for i in items:
+        e = pq(i)
+
+        m = Movie()
+        m.name = e('.info .title').text().replace(' /', '/')
+        m.score = e('.rating_num').text()
+        m.quote = e('.inq').text()
+        m.hot = e('.star').text().replace(f'{m.score} ', '')
+        m.cover_url = e('div.pic img').attr('src')
+        save_pic(m)
+
+        m.save()
+        log(m)
+
+
+def save_pic(movie):
+    fname = movie.name.split('/')[0] + '.jpg'
+    fname = fname.replace(' ', '')
+
+    cached_url(movie.cover_url, fname)
 
 
 def cached_url(url, filename):
@@ -38,22 +71,16 @@ def cached_url(url, filename):
     if not os.path.exists(folder):
         os.makedirs(folder)
 
-    path = os.path.join(folder, filename)
+    path = os.path.join(folder, fname)
     if os.path.exists(path):
         with open(path, 'rb') as f:
             s = f.read()
             return s
     else:
-        r = requests.get(url)
+        r = requests.get(u)
         with open(path, 'wb') as f:
             f.write(r.content)
             return r.content
-
-
-def save_pic(movies):
-    for path in movies:
-        filename = f'{path.name}.png'
-        cached_url(path.cover_url, filename)
 
 
 def get_all_page_url():
@@ -69,8 +96,8 @@ def __main():
     for i, u in enumerate(url):
         log(f'URL is {u}')
 
-        content = cached_url(u, f'page{i}')
-        print(collect_data(content))
+        content = cached_url(u, f'page{i}').replace(b'&nbsp;', b'')
+        collect_data(content)
 
 
 if __name__ == '__main__':
